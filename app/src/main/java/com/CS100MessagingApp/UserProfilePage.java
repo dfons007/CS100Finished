54b6136ae101c9c7e6804aaefd785c45de91d0cf
dfons007 <dfons007@ucr.edu>
2018-05-01 14:33:53 -0700

    Placed Login button functionality in the correct folder.

--- app/src/main/java/com/CS100MessagingApp/UserProfilePage.java
+++ app/src/main/java/com/CS100MessagingApp/UserProfilePage.java
@@ -1,27 +1,17 @@
 package com.CS100MessagingApp;
 
 
-import android.app.ProgressDialog;
-import android.content.Intent;
+=import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
-
-import org.json.JSONException;
-import org.json.JSONObject;
 
 public class UserProfilePage extends AppCompatActivity {
 
-    Button ChangePassword;
+    Button ChangePassword, logoutButton;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
@@ -31,15 +21,25 @@ public class UserProfilePage extends AppCompatActivity {
         AlphaAnimation fadein = new AlphaAnimation(0.0f,1.0f);
 
         ChangePassword = (Button)findViewById(R.id.button_changepw);
+        logoutButton = (Button)findViewById(R.id.button_logout);
+
 
         fadein.setDuration(1500);
         fadein.setFillAfter(true);
         ChangePassword.startAnimation(fadein);
-
+        logoutButton.startAnimation(fadein);
         ChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 startActivity(new Intent(UserProfilePage.this,ResetPassword.class));
+            }
+        });
+
+        logoutButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),Login.class);
+                startActivity(i);
             }
         });
     }
