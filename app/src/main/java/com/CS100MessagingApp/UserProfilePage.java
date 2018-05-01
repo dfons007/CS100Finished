2427cbffa33e9b11aced194a1c6fa2a28bea9809
LXY123587 <31035010+LXY123587@users.noreply.github.com>
2018-05-01 08:02:02 +0800

    functionality of Change Password button is added

--- app/src/main/java/com/CS100MessagingApp/UserProfilePage.java
+++ app/src/main/java/com/CS100MessagingApp/UserProfilePage.java
@@ -1,25 +1,46 @@
 package com.CS100MessagingApp;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Button;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.view.animation.AlphaAnimation;
+import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class UserProfilePage extends AppCompatActivity {
 
+    Button ChangePassword;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profilepage);
 
+        AlphaAnimation fadein = new AlphaAnimation(0.0f,1.0f);
 
+        ChangePassword = (Button)findViewById(R.id.button_changepw);
+
+        fadein.setDuration(1500);
+        fadein.setFillAfter(true);
+        ChangePassword.startAnimation(fadein);
+
+        ChangePassword.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(UserProfilePage.this,ResetPassword.class));
+            }
+        });
     }
 }
