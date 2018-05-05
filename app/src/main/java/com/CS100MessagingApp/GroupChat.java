43b58edc8984144dc38bc47f9ca3c5aa5e3b59cd
Daniel Fonseca <dfonseca355@student.msjc.edu>
2018-05-05 01:54:29 -0700

    Base code for the GroupChat Class

---
+++ app/src/main/java/com/CS100MessagingApp/GroupChat.java
@@ -0,0 +1,38 @@
+package com.CS100MessagingApp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.ScrollView;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class GroupChat extends AppCompatActivity {
+    LinearLayout layout;
+    RelativeLayout layout_2;
+    ImageView sendButton;
+    EditText messageArea;
+    ScrollView scrollview;
+    DatabaseReference groupRef;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat);
+
+        layout = (LinearLayout) findViewById(R.id.layout1);
+        layout_2 = (RelativeLayout)findViewById(R.id.layout2);
+        sendButton = (ImageView)findViewById(R.id.sendButton);
+        messageArea = (EditText)findViewById(R.id.messageArea);
+        scrollview = (ScrollView)findViewById(R.id.scrollView);
+
+        // Reference to GroupChat Message Data
+        groupRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://messaging-app-cs100.firebaseio.com/messages/"+UserDetails.CurrentGroupChat);
+
+    }
+
+}
