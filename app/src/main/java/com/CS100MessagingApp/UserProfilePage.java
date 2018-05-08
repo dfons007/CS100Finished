a7fff9b53adc194a8de2fcee8e686759b1533e0e
dfons007 <dfons007@ucr.edu>
2018-05-07 18:20:49 -0700

    Adding Files to Correct Location

--- app/src/main/java/com/CS100MessagingApp/UserProfilePage.java
+++ app/src/main/java/com/CS100MessagingApp/UserProfilePage.java
@@ -1,38 +1,42 @@
 package com.CS100MessagingApp;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+
 import android.view.View;
 import android.view.animation.AlphaAnimation;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
-import android.net.Uri;
 import android.widget.TextView;
 import android.widget.Toast;
-import android.app.ProgressDialog;
 
-
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.bumptech.glide.Glide;
-import com.bumptech.glide.request.RequestOptions;
-import com.bumptech.glide.load.DataSource;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-
-import com.bumptech.glide.request.RequestOptions;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.StorageReference;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 
 
-import java.io.ByteArrayOutputStream;
-
 public class UserProfilePage extends AppCompatActivity {
 
-    Button ChangePassword, logoutButton, chatButton;
-    ImageView profilepicture;
+    Button ChangePassword, logoutButton;
+    ImageView profilepicture,EditBioImage;
     TextView profilename;
+    EditText profile_bio;
     String usersName = UserDetails.username;
+    String userBio = UserDetails.bio;
     private static int GALLERY_INTENT = 2;
     private StorageReference storage;
     private ProgressDialog pd;
@@ -53,8 +57,13 @@ public class UserProfilePage extends AppCompatActivity {
         logoutButton = (Button)findViewById(R.id.button_logout);
         profilepicture = (ImageView)findViewById(R.id.ProPic);
         profilename =(TextView)findViewById(R.id.ProfilePicName);
+        profile_bio = (EditText) findViewById(R.id.bio_textView);
         profilename.setText(usersName);
-        chatButton =(Button)findViewById(R.id.button_chat);
+        EditBioImage = (ImageView)findViewById(R.id.Edit_imageView);
+
+        if (!UserDetails.bio.isEmpty()){
+            profile_bio.setText(UserDetails.bio);
+        }
 
         pd = new ProgressDialog(this);
 
@@ -68,8 +77,7 @@ public class UserProfilePage extends AppCompatActivity {
         fadein.setFillAfter(true);
         ChangePassword.startAnimation(fadein);
         logoutButton.startAnimation(fadein);
-        chatButton.startAnimation(fadein);
-        // Animation Endl
+        // Animation End
 
         ChangePassword.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -96,11 +104,38 @@ public class UserProfilePage extends AppCompatActivity {
 
         });
 
-        chatButton.setOnClickListener(new View.OnClickListener() {
+
+        EditBioImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(),Users.class);
-                startActivity(intent);
+                final ProgressDialog pd = new ProgressDialog(UserProfilePage.this);
+                pd.setMessage("Update Bio..");
+                pd.show();
+                // URL to firebase user data json
+                String url = "https://messaging-app-cs100.firebaseio.com/users.json";
+                // Request to firebase user data json
+                StringRequest request = new StringRequest(Request.Method.GET, url, new Response.Listener<String>(){
+                    @Override
+                    public void onResponse(String s) {
+                        if(s.equals("null")){
+                            Toast.makeText(UserProfilePage.this, "Fail to request from Firebase", Toast.LENGTH_LONG).show();
+                        }
+                        else{
+                                DatabaseReference reference = FirebaseDatabase.getInstance().getReferenceFromUrl("https://messaging-app-cs100.firebaseio.com/users");
+                                reference.child(usersName).child("bio").setValue(profile_bio.getText().toString());
+                                Toast.makeText(UserProfilePage.this,"Update Bio successfully", Toast.LENGTH_LONG).show();
+                        }
+                        pd.dismiss();
+                    }
+                },new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError volleyError) {
+                        System.out.println("" + volleyError);
+                        pd.dismiss();
+                    }
+                });
+                RequestQueue rQueue = Volley.newRequestQueue(UserProfilePage.this);
+                rQueue.add(request);
             }
         });
     }
