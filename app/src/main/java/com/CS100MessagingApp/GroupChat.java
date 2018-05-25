4665c0779b3ababf4a0f45d48854fb7564a624e1
Shuofeng-Wang <38358392+Shuofeng-Wang@users.noreply.github.com>
2018-05-25 03:47:13 -0700

    Users can send images in group chat now.
    Users can send images in group chat now. And the images sent in group chat will have a unique name in Firebase to prevent a bug found before.

--- app/src/main/java/com/CS100MessagingApp/GroupChat.java
+++ app/src/main/java/com/CS100MessagingApp/GroupChat.java
@@ -1,40 +1,43 @@
 package com.CS100MessagingApp;
 
 import android.os.Bundle;
-import android.provider.ContactsContract;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Adapter;
-import android.widget.AdapterView;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
-import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
-import android.widget.SimpleCursorAdapter;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.RequestOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class GroupChat extends AppCompatActivity {
     LinearLayout layout;
     RelativeLayout layout_2;
-    ImageView sendButton;
+    ImageView sendButton,sendPicture;
     EditText messageArea;
     ScrollView scrollview;
     DatabaseReference groupRef;
+    private StorageReference imageRef;
+    private static int GALLERY_INTENT = 2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,10 +47,12 @@ public class GroupChat extends AppCompatActivity {
         layout = (LinearLayout) findViewById(R.id.layout1);
         layout_2 = (RelativeLayout)findViewById(R.id.layout2);
         sendButton = (ImageView)findViewById(R.id.sendButton);
+        sendPicture = (ImageView)findViewById(R.id.sendPicture);
         messageArea = (EditText)findViewById(R.id.messageArea);
         scrollview = (ScrollView)findViewById(R.id.scrollView);
 
         groupRef = FirebaseDatabase.getInstance().getReferenceFromUrl("https://messaging-app-cs100.firebaseio.com/messages/" + UserDetails.CurrentGroup);
+        imageRef = FirebaseStorage.getInstance().getReference();
 
         sendButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,10 +63,21 @@ public class GroupChat extends AppCompatActivity {
                     Map<String, String> map = new HashMap<String, String>();
                     map.put("user", UserDetails.username);
                     map.put("message", messageText);
+                    map.put("type","text");
 
                     groupRef.push().setValue(map);
                     messageArea.setText("");
                 }
+            }
+        });
+
+        sendPicture.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                android.content.Intent intent = new android.content.Intent(android.content.Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent,2);
+
             }
         });
 
@@ -71,12 +87,31 @@ public class GroupChat extends AppCompatActivity {
                 Map<String,Object>map = (Map<String,Object>)dataSnapshot.getValue();
                 String message = map.get("message").toString();
                 String userName = map.get("user").toString();
+                String type;
+
+                if(map.containsKey("type")){
+                    type = map.get("type").toString();
+                }else{
+                    type = "text";
+                }
 
                 if(userName.equals(UserDetails.username)){
-                    addMessageBox("You:\n" + message, 1);
+                    if(type.equals("image"))
+                    {
+                        //In this case if you use "addMessageBox("You:\n"+message,1,type);" It will lead to bug.
+                        addMessageBox(message,1,type);
+                    }else {
+                        addMessageBox("You:\n" + message, 1,type);
+                    }
                 }
                 else{
-                    addMessageBox(userName + ":\n" + message, 2);
+                    if (type.equals("text")){
+                        addMessageBox(userName+":\n"+message, 2, type);
+                    }else {
+                        //The type of message is image
+                        addMessageBox(userName+":\nI send an image.", 2, "text");
+                        addMessageBox(message, 2, type);
+                    }
                 }
             }
 
@@ -102,23 +137,109 @@ public class GroupChat extends AppCompatActivity {
         });
     }
 
-    public void addMessageBox(String message, int type){
+    /**
+     * @param type : type==1 means that the message is sent by you, and type==2 means that it is sent by your group chat members;
+     * @param MessageType : It indicates the type of the current message;
+     */
+    public void addMessageBox(String message, int type, String MessageType){
         TextView textView = new TextView(GroupChat.this);
-        textView.setText(message);
+        ImageView myImage = new ImageView(GroupChat.this);
+        //textView.setText(message);
+
+        if(MessageType.equals("text")){
+            textView.setText(message);
+            myImage.setVisibility(View.GONE);
+        }else if (MessageType.equals("image"))
+        {
+            textView.setVisibility(View.GONE);
+            myImage.setVisibility(View.VISIBLE);
+        }
 
         LinearLayout.LayoutParams lp2 = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         lp2.weight = 1.0f;
 
+        // If the message is sent by you
         if(type == 1) {
-            lp2.gravity = Gravity.LEFT;
-            textView.setBackgroundResource(R.drawable.bubble_out);
+            if(MessageType.equals("text")){
+                //The message type is text
+                lp2.gravity = Gravity.LEFT;
+                textView.setBackgroundResource(R.drawable.bubble_out);
+                textView.setLayoutParams(lp2);
+                layout.addView(textView);
+                scrollview.fullScroll(View.FOCUS_DOWN);
+                myImage.setVisibility(View.GONE);
+            }else{
+                // The message type is Image
+                lp2.gravity = Gravity.LEFT;
+                myImage.setBackgroundResource(R.drawable.bubble_out);
+                Glide.with(this)
+                        .load(message)
+                        .apply(new RequestOptions()
+                                .override(190,300).centerCrop())
+                        .into(myImage);
+                myImage.setLayoutParams(lp2);
+                layout.addView(myImage);
+                scrollview.fullScroll(View.FOCUS_DOWN);
+            }
+
         }
         else{
-            lp2.gravity = Gravity.RIGHT;
-            textView.setBackgroundResource(R.drawable.bubble_in);
+            //If the message is sent by other people
+            if (MessageType.equals("text")){
+                //The message type is text
+                lp2.gravity = Gravity.RIGHT;
+                textView.setBackgroundResource(R.drawable.bubble_in);
+                textView.setLayoutParams(lp2);
+                layout.addView(textView);
+                scrollview.fullScroll(View.FOCUS_DOWN);
+                myImage.setVisibility(View.GONE);
+            }else {
+                // The message type is Image
+                lp2.gravity = Gravity.RIGHT;
+                myImage.setBackgroundResource(R.drawable.bubble_in);
+                Glide.with(this)
+                        .load(message)
+                        .apply(new RequestOptions()
+                                .override(190,300).centerCrop())
+                        .into(myImage);
+                myImage.setLayoutParams(lp2);
+                layout.addView(myImage);
+                scrollview.fullScroll(View.FOCUS_DOWN);
+            }
+           }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, android.content.Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == GALLERY_INTENT && resultCode == RESULT_OK){
+            // Get URI for image picked from gallery.
+            android.net.Uri sentImageURI = data.getData();
+            // Gets ID for what the image will be called.
+            final Map messageMap = new HashMap();
+            final String push_id = String.valueOf(System.identityHashCode(messageMap));
+            //Gets the Filepath for the Firebase Storage.
+            StorageReference filepath = imageRef.child("message_images").child(groupRef.getKey()).child(push_id+".jpg");
+            //Puts the the image into the Firebase Storage.
+            filepath.putFile(sentImageURI).addOnCompleteListener(new OnCompleteListener<UploadTask.TaskSnapshot>(){
+                @Override
+                public void onComplete(@NonNull Task<UploadTask.TaskSnapshot> task){
+                    if(task.isSuccessful()){
+                        //Gets a URL for the Image.
+                        String download_url = task.getResult().getDownloadUrl().toString();
+                        // Sends the Image as message into the Firebase RealTime Database.
+                        messageMap.put("message",download_url);
+                        messageMap.put("user", UserDetails.username);
+                        messageMap.put("type","image");
+                        groupRef.push().setValue(messageMap);
+                    }
+                }
+
+
+            });
+
+
         }
-        textView.setLayoutParams(lp2);
-        layout.addView(textView);
-        scrollview.fullScroll(View.FOCUS_DOWN);
+
     }
 }
